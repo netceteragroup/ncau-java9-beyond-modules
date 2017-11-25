8ccef486265def35e18edc33b87c7056eadb99fb
Michael Pellaton <michael.pellaton@gmail.com>
2017-11-25 20:03:38 +0100

    added test showcasing the collection factory methods added in Java 9

---
+++ newwith9/src/test/java/com/netcetera/ncau/java9beyondmodules/newwith9/CollectionsTest.java
@@ -0,0 +1,42 @@
+package com.netcetera.ncau.java9beyondmodules.newwith9;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests showing the new collection factory methods in Java 9.
+ */
+class CollectionsTest {
+
+  @Test
+  void setOf() {
+    assertThat(Set.of(1, 2, 3), containsInAnyOrder(1, 2, 3));
+    assertThrows(UnsupportedOperationException.class, () -> Set.of(1).add(2));
+  }
+
+  @Test
+  void listOf() {
+    assertThat(List.of(1, 2, 3), containsInAnyOrder(1, 2, 3));
+    assertThrows(UnsupportedOperationException.class, () -> List.of(1).add(2));
+  }
+
+  @Test
+  void mapOf() {
+    assertThat(Map.of(1, 'A', 2, 'B'), Matchers.hasEntry(1, 'A'));
+    assertThrows(UnsupportedOperationException.class, () -> Map.of(1, 'A').put(2, 'B'));
+  }
+
+  @Test
+  void mapOfEntries() {
+    assertThat(Map.ofEntries(Map.entry(1, 'A'), Map.entry(2, 'B')), Matchers.hasEntry(1, 'A'));
+    assertThrows(UnsupportedOperationException.class, () -> Map.ofEntries(Map.entry(1, 'A')).put(2, 'B'));
+  }
+}
--- newwith9/src/test/java/com/netcetera/ncau/java9beyondmodules/newwith9/ObjectsTest.java
+++ newwith9/src/test/java/com/netcetera/ncau/java9beyondmodules/newwith9/ObjectsTest.java
@@ -13,19 +13,19 @@ import org.junit.jupiter.api.Test;
  * Tests showing the new methods on {@link java.lang.Object}.
  */
 class ObjectsTest {
-  
+
   @Test
   void requireNonNullElse() {
     assertEquals("default", Objects.requireNonNullElse(null, "default"));
     assertEquals("value", Objects.requireNonNullElse("value", "default"));
   }
-  
+
   @Test
   void requireNonNullElseGet() {
     assertEquals("default", Objects.requireNonNullElseGet(null, () -> "default"));
     assertEquals("value", Objects.requireNonNullElseGet("value", () -> "default"));
   }
-  
+
   @Test
   void checkIndex() {
     assertEquals(0, Objects.checkIndex(0, 10));
@@ -40,15 +40,15 @@ class ObjectsTest {
     assertEquals(0, Objects.checkFromToIndex(0, 9, 10));
     assertThrows(IndexOutOfBoundsException.class, () -> Objects.checkFromToIndex(9, 11, 10));
   }
-  
+
   @Test
   void checkFromIndexSize() {
     assertEquals(0, Objects.checkFromIndexSize(0, 10, 10));
     assertThrows(IndexOutOfBoundsException.class, () -> Objects.checkFromIndexSize(9, 2, 10));
   }
-  
+
   /**
-   * The {@code isNull(Object object)} method was added in Java 1.8 and it is 
+   * The {@code isNull(Object object)} method was added in Java 1.8 and it is
    * repeated here because its usage is not as high as the author wishes it to be.
    */
   @Test
@@ -58,7 +58,7 @@ class ObjectsTest {
   }
 
   /**
-   * The {@code nonNull(Object object)} method was added in Java 1.8 and it is 
+   * The {@code nonNull(Object object)} method was added in Java 1.8 and it is
    * repeated here because its usage is not as high as the author wishes it to be.
    */
   @Test
@@ -66,15 +66,15 @@ class ObjectsTest {
     assertFalse(Objects.nonNull(null));
     assertTrue(Objects.nonNull(""));
   }
-  
+
   /**
-   * The {@code requireNonNull(Object object, Supplier<String> supplier)} method was added in 
-   * Java 1.8 and it is repeated here because its usage is not as high as the author wishes it to be.
+   * The {@code requireNonNull(Object object, Supplier<String> supplier)} method
+   * was added in Java 1.8 and it is repeated here because its usage is not as
+   * high as the author wishes it to be.
    */
   @Test
   void requireNonNull() {
     assertThrows(NullPointerException.class, () -> Objects.requireNonNull(null, () -> "message"), "message");
     assertEquals("value", Objects.requireNonNull("value", () -> "message"));
   }
-  
 }
