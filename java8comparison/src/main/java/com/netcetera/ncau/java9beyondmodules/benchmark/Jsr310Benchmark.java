a0acd493e165090b0cc79782fabbd9fd2f2bfd8a
Philippe Marschall <philippe.marschall@gmail.com>
2017-12-23 17:54:03 +0100

    Add LocalDate#format
    Add benchmark for LocalDate#format and comparison with old API.

--- java8comparison/src/main/java/com/netcetera/ncau/java9beyondmodules/benchmark/Jsr310Benchmark.java
+++ java8comparison/src/main/java/com/netcetera/ncau/java9beyondmodules/benchmark/Jsr310Benchmark.java
@@ -1,16 +1,23 @@
 package com.netcetera.ncau.java9beyondmodules.benchmark;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 
 /**
  * Demonstrates various performance improvements made to the JSR-310
@@ -18,11 +25,25 @@ import java.util.concurrent.TimeUnit;
  */
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Benchmark)
 public class Jsr310Benchmark {
 
   private static final LocalDate LOCAL_DATE = LocalDate.of(2017, 11, 30);
 
   private static final LocalDateTime LOCAL_DATE_TIME = LocalDateTime.of(LOCAL_DATE, LocalTime.of(16, 0));
+
+  private Date date;
+
+  private SimpleDateFormat simpleDateFormat;
+
+  /**
+   * Set up the benchmark state.
+   */
+  @Setup
+  public void setup() {
+    this.date = Date.from(LOCAL_DATE.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    this.simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd");
+  }
 
   /**
    * Demonstrates the performance improvements made to
@@ -46,6 +67,41 @@ public class Jsr310Benchmark {
   @Benchmark
   public LocalDate plusDays() {
     return LOCAL_DATE.plusDays(1L);
+  }
+
+  /**
+   * Demonstrates the performance of {@link java.util.Calendar#add(int, int)}
+   * compared to JSR-310.
+   *
+   * @return object to black hole
+   */
+  @Benchmark
+  public Date calendarAddDays() {
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(date);
+    calendar.add(Calendar.DAY_OF_MONTH, 1);
+    return calendar.getTime();
+  }
+
+  /**
+   * Demonstrates formatting performance improvements.
+   *
+   * @return object to black hole
+   */
+  @Benchmark
+  public String format() {
+    return LOCAL_DATE.format(DateTimeFormatter.ISO_DATE);
+  }
+
+  /**
+   * Demonstrates SimpleDateFormat formatting performance compared to JSR-310
+   * performance.
+   *
+   * @return object to black hole
+   */
+  @Benchmark
+  public String simapleDateFormat() {
+    return this.simpleDateFormat.format(this.date);
   }
 
 }
